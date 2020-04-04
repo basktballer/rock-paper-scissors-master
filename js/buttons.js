bee1741ebc6ac32f2f9a12bdb4ae0df02f9b3781
Eden Yeung <basktballer@gmail.com>
2020-04-03 20:58:38 -0400

    fixed up logic, start to play with gameplay screens

--- js/buttons.js
+++ js/buttons.js
@@ -11,38 +11,33 @@ const spockButton = document.querySelector(".spock-div");
 const rulesButton = document.querySelector(".rules-button");
 const rulesPopup = document.querySelector(".rules-popup");
 const closeButton = document.querySelector(".close-button");
+const selectionLayout = document.querySelector(".selection-layout")
+const gameLayout = document.querySelector(".game-layout")
 
 const scoreOutput = document.getElementById("score-output");
 let score = 0;
+let winner = "";
+
+
 
 rockButton.addEventListener('click', function (event) {
-  score = scoreOutput.innerHTML;
-  score++;
-  scoreOutput.innerHTML = score;
+  playGame('rock');
 });
 
 paperButton.addEventListener('click', function (event) {
-  score = scoreOutput.innerHTML;
-  score++;
-  scoreOutput.innerHTML = score;
+  playGame('paper');
 });
 
 scissorsButton.addEventListener('click', function (event) {
-  score = scoreOutput.innerHTML;
-  score++;
-  scoreOutput.innerHTML = score;
+  playGame('scissors');
 });
 
 lizardButton.addEventListener('click', function (event) {
-  score = scoreOutput.innerHTML;
-  score++;
-  scoreOutput.innerHTML = score;
+  playGame('lizard');
 });
 
 spockButton.addEventListener('click', function (event) {
-  score = scoreOutput.innerHTML;
-  score++;
-  scoreOutput.innerHTML = score;
+  playGame('spock');
 });
 
 rulesButton.addEventListener('click', function (event) {
@@ -52,3 +47,75 @@ rulesButton.addEventListener('click', function (event) {
 closeButton.addEventListener('click', function (event) {
   rulesPopup.style.display = "none";
 })
+
+async function playGame(selection) {
+  let choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+  let houseChoice = '';
+
+  selectionLayout.style.display = "none";
+  gameLayout.style.display = "block";
+  
+  console.log(`user selected ${selection}, waiting 1 seconds for computer to pick`)
+  document.querySelector(".user-icon").src=`./images/icon-${selection}.svg`;
+  document.querySelectorAll(".user, ")
+  element.classList.add("my-class")
+  
+  await new Promise(r => setTimeout(r, 1000));
+  
+  houseChoice = choices[Math.floor(Math.random() * 4)];
+  console.log(`house selected ${houseChoice}, waiting 1 seconds and evaluate`)
+  document.querySelector(".house-icon").src=`./images/icon-${houseChoice}.svg`;
+
+  await new Promise(r => setTimeout(r, 1000));
+
+  winner = evaluateChoices(selection, houseChoice);
+
+  if (winner = 'user') {
+    console.log(`${selection} beats ${houseChoice}. User wins!`)
+    score = scoreOutput.innerHTML;
+    score++;
+    scoreOutput.innerHTML = score;
+  } else {
+
+    console.log(`${houseChoice} beats ${selection}. House wins!`)
+  }
+
+}
+
+function evaluateChoices(user, house) {
+
+  if (user === house) {
+    return 'draw';
+  }
+  if(user === 'rock') {
+    if (house === 'lizard' || house === 'scissors') {
+      return 'user'
+    } else {
+      return 'house'
+    }         
+  } else if (user === 'paper') {
+    if (house === 'rock' || house === 'spock') {
+      return 'user'
+    } else {
+      return 'house'
+    }
+  } else if (user === 'scissors') {
+    if (house === 'lizard' || house === 'paper') {
+      return 'user'
+    } else {
+      return 'house'
+    }
+  } else if (user === 'lizard') {
+    if (house === 'spock' || house === 'paper') {
+      return 'user'
+    } else {
+      return 'house'
+    }
+  } else if (user === 'spock') {
+    if (house === 'rock' || house === 'scissors') {
+      return 'user'
+    } else {
+      return 'house'
+    }
+  }
+}
