a2a41476aa002c1548378970651766b34f5a5a61
Eden Yeung <basktballer@gmail.com>
2020-04-04 23:15:57 -0400

    Mobile fully functional

--- js/buttons.js
+++ js/buttons.js
@@ -67,7 +67,7 @@ async function playGame(selection) {
   selectionLayout.style.display = "none";
   gameLayout.style.display = "block";
   userChoice = selection;
-  
+
   console.log(`user selected ${selection}, waiting 1 seconds for computer to pick`)
   userIcon.src=`./images/icon-${selection}.svg`;
   userDiv.classList.add(`${selection}-div`)
@@ -87,12 +87,16 @@ async function playGame(selection) {
 
   if (winner === 'user') {
     victoryText.style.display = "block";
+    userDiv.classList.add('winner-box-shadow');
+    userDiv.parentElement.classList.add('winner-fade-back');
     console.log(`${selection} beats ${houseChoice}. User wins!`)
     score = scoreOutput.innerHTML;
     score++;
     scoreOutput.innerHTML = score;
   } else if (winner === 'house'){
     defeatText.style.display = "block";
+    houseDiv.classList.add('winner-box-shadow');
+    houseDiv.parentElement.classList.add('winner-fade-back');
     console.log(`${houseChoice} beats ${selection}. House wins!`)
   } else {
     drawText.style.display = "block";
@@ -150,7 +154,10 @@ function resetElements() {
   selectionLayout.style.display = "grid";
   houseIcon.src="";
   houseIcon.style.display = "none";
-  houseDiv.classList.remove(`${houseChoice}-div`)
+  houseDiv.classList.remove(`${houseChoice}-div`, 'winner-box-shadow');
   userIcon.src="";
-  userDiv.classList.remove(`${userChoice}-div`)
+  userDiv.classList.remove(`${userChoice}-div`, 'winner-box-shadow');
+  userDiv.parentElement.classList.remove('winner-fade-back');
+  houseDiv.parentElement.classList.remove('winner-fade-back');
+
 }
